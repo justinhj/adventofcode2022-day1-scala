82f8b91a027f2201de7bdb56496adb8de8bce6a8
Justin Heyes-Jones <justinhj@gmail.com>
2022-12-01 20:50:58 -0800

    project

--- src/main/scala/org/justinhj/Adventofcode2022day1.scala
+++ src/main/scala/org/justinhj/Adventofcode2022day1.scala
@@ -1,7 +1,5 @@
 package org.justinhj
 
-import zio.ZQueue
-
 import scala.io.Source
 import scala.util.Try
 
@@ -15,31 +13,42 @@ object Adventofcode2021day1 extends App {
 
   import AOCUtil.inputToStrings
 
-  def parseInput(input: List[String]): List[Line] = {
-    input.map {
-      line =>
-        val pattern = """(\d+),(\d+) -> (\d+).(\d+)""".r
+  def parseInput(input: List[String]): List[Option[Int]] = 
+    input.map {a => Try(a.toInt).toOption}
 
-        val parsed = Try {
-          val pattern(x1,y1,x2,y2) = line
-          Line(Vec2(x1.toInt,y1.toInt),Vec2(x2.toInt,y2.toInt))
-        }
-        parsed.get
+  def splitBy[A](p: A => Boolean, input: List[A]): List[List[A]] = {
+    val out = input.foldLeft((List.empty[List[A]], List.empty[A])) {
+      case (((acc, cur), a)) =>
+        if(!p(a)) (acc, a +: cur)
+        else (cur :: acc, List.empty[A])
     }
+    out._1 :+ out._2
   }
 
-  // def solve(lines: List[Line], allowDiagonal: Boolean): Int = {
-  //   val allPoints = lines
-  //                     .flatMap(p => createLine(p,allowDiagonal))
-  //                     .groupMapReduce(identity)(_ => 1)(_ + _)
+  def solve(input: List[Option[Int]]): Int = {
+    val groups = splitBy((a: Option[Int]) => a.isEmpty, input)
+    val flattened = groups.map(e => e.flatten)
+    flattened.map(_.sum).max
+  }
 
-  //   allPoints.values.count(_ > 1)
-  // }
-
+  // Part 1 - elf with most calories
   val exampleInput = inputToStrings("example.txt")
   val exampleLines = parseInput(exampleInput)
-  // val exampleSolution = solve(exampleLines,allowDiagonal = false)
-  // println(exampleSolution)
-  // val exampleSolutionPart2 = solve(exampleLines,allowDiagonal = true)
-  // println(exampleSolutionPart2)
+  println(solve(exampleLines))
+
+  val input1 = inputToStrings("day1.txt")
+  val input1Lines = parseInput(input1)
+  println(solve(input1Lines))
+
+  // Part 2 - top 3 elves
+  // Can just modify the solve function a bit
+
+  def solve2(input: List[Option[Int]]): Int = {
+    val groups = splitBy((a: Option[Int]) => a.isEmpty, input)
+    val flattened = groups.map(e => e.flatten)
+    flattened.map(_.sum).sorted.reverse.take(3).sum
+  }
+
+  println(solve2(exampleLines))
+  println(solve2(input1Lines))
 }
